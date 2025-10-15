f5bcfd99b630d885f694182b68fd32666023605a
FARIYA <fariyaa2001@gmail.com>
2025-10-15 21:59:24 +0600

    fix the calender icone

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const resolvedList = document.getElementById("resolvedList");
 const inProgressCount = document.getElementById("inProgressCount");
 const resolvedCount = document.getElementById("resolvedCount");
 
-// ===== Helper: Calendar Icon (Slightly Lowered) =====
+// ===== Calendar Icon=====
 function calendarSVG() {
   return `
     <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4 mr-1 relative top-[1.5px] align-text-bottom" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
